fbd0ef4809983bc188f521606bd942cb13987af7
baohq <huynhquocbaoit@gmail.com>
2019-06-05 21:16:57 +0700

    Create vue components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,19 +4,45 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+/* 
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+ */
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+/* Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
 });
+ */
+
+ import Vue from 'vue'
+ import VueRouter from 'vue-router'
+ import Hompage from './components/Homepage'
+ import Read from './components/Read'
+
+ Vue.use(VueRouter)
+
+ const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/admin/dashboard',
+            name: 'read',
+            component: Read,
+            props: true
+        },
+    ],
+ });
+
+ const app = new Vue({
+    el: '#app',
+    router,
+    components: { Hompage },
+ });
