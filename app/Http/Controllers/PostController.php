492919eba4e4550d151bbf85dcc2e3278024df71
baohq <huynhquocbaoit@gmail.com>
2019-06-08 12:09:35 +0700

    Fix show post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,7 +70,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return PostResource($post);
+        return new PostResource($post);
     }
 
     /**
